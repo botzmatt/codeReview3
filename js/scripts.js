1306313713a1d45cf056916eb2efb0df002f9d82
Lindsay Botz <Lindsay Botz>
2016-06-25 13:03:12 -0700

    fixes margin of the sidebar column and adds first bit of business logic for number being divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+// Business End Logic
 
+  function pingPong(input) {
+    for (var i=1; i <= input; i+=1) {
+      if(i % 15 === 0) {
+        $("ul#numberOutput").append("<li>pingpong</li>");
+      }
+    }
+  }
